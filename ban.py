ea8f3474b3cff559b9228a916ae59b94c7627cb7
wubby <65384601+wub-wub@users.noreply.github.com>
2022-09-07 11:36:51 +0100

    Create ban.py

---
+++ ban.py
@@ -0,0 +1,18 @@
+import discord
+from discord.ext import commands
+
+bot = commands.Bot(command_prefix="your prefix here") # add a prefix for your bot, EG. "<" or "!" 
+
+token = "your token here" #add your discord bots token here
+
+@bot.event
+async def on_ready():
+    print ("the bot is ready!")
+
+@bot.command()
+@commands.has_permissions(ban_members=True)
+async def ban(ctx, member:discord.Member, *,reason="Not defined"):
+    await ctx.send(f"{member.mention} has been banned from {ctx.guild} by {ctx.author} for {reason}")
+    await member.ban(reason=reason)
+    
+bot.run(token)
